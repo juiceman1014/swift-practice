f57b18a82b483c4f917a1a3a47c62610ba2ea4cb
juiceman1014 <justintran1014@gmail.com>
2024-09-15 23:02:38 -0700

    initial commit

---
+++ UIPractice/UIPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UIPractice
+//
+//  Created by Justin Tran on 9/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("I will be doing user interface practice here!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UIPractice/UIPractice/UIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIPracticeApp.swift
+//  UIPractice
+//
+//  Created by Justin Tran on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct UIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

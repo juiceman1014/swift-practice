41dba1552a1b5ac68f074cdb1f1d1acd0f22e3cf
juiceman1014 <justintran1014@gmail.com>
2024-09-16 21:32:51 -0700

    button that allows you to increment a counter

--- UIPractice/UIPractice/ContentView.swift
+++ UIPractice/UIPractice/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter = 0
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("I will be doing user interface practice here!")
+            
+            Text("Counter: \(counter)")
+                .padding()
+            
+            Button(action:{
+                counter += 1
+            }){
+                Text("Increment Counter")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
         }
         .padding()
     }

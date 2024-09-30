16b43cb977c9f8e731e79ed4178b94608fbfa819
juiceman1014 <justintran1014@gmail.com>
2024-09-29 21:09:58 -0700

    scrolling up and down using ScrollView

--- UIPractice/UIPractice/ContentView.swift
+++ UIPractice/UIPractice/ContentView.swift
@@ -8,56 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var counter = 0
+    
     var body: some View {
-        VStack {
-            
-            HStack{
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                
-                Image(systemName: "star.fill")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                
+        ScrollView{
+            VStack {
+                Image("cars")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                Image("cars")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                Image("cars")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                Image("cars")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                Image("cars")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
                 Image("cars")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
             }
             .padding()
-                
-                
-            
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            
-            Text("Counter: \(counter)")
-                .padding()
-            
-            Button(action:{
-                counter += 1
-            }){
-                Text("Increment Counter")
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
-            
-            Button(action:{
-                counter -= 1
-            }){
-                Text("Decrement Counter")
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
         }
-        .padding()
     }
 }
 

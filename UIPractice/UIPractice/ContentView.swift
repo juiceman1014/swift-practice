f4c52f36de6dbc4f858dde96216f5b479ff3ce13
juiceman1014 <justintran1014@gmail.com>
2024-09-30 22:08:36 -0700

    navigating between multiple pages

--- UIPractice/UIPractice/ContentView.swift
+++ UIPractice/UIPractice/ContentView.swift
@@ -11,16 +11,20 @@ struct ContentView: View {
     let images = ["car", "car2", "car3"]
     
     var body: some View {
-        TabView{
-            ForEach(images, id: \.self){ image in
-                Image(image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
+        NavigationStack{
+            VStack{
+                Text("Choose an Image")
+                    .font(.largeTitle)
+                    .padding()
+                
+                NavigationLink("Go to Image 1", destination: ImageView1())
+                    .padding()
+                
+                NavigationLink("Go to Image 2", destination: ImageView2())
                     .padding()
             }
+            .navigationTitle("Main Menu")
         }
-        .tabViewStyle(PageTabViewStyle())
     }
 }
 
